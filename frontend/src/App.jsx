828b8a31475625a35e72d1ee11f809dd5a1e1998
Wiktoria Kudła <262254@student.pwr.edu.pl>
2026-06-06 21:37:01 +0200

    visual changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,29 @@ function App() {
   const [zakladka, setZakladka] = useState('home')
 
   return (
-    <div>
-      <nav>
-        <button onClick={() => setZakladka('home')}>🏠 Start</button>
-        <button onClick={() => setZakladka('test')}>🧠 Test osobowości</button>
-        <button onClick={() => setZakladka('kalendarz')}>📅 Kalendarz</button>
-        <button onClick={() => setZakladka('listy')}>💌 Listy</button>
-        <button onClick={() => setZakladka('zadanie')}>Zadanie</button>
-      </nav>
+    <div className="min-h-screen bg-taupe-500 text-stone-800 font-serif">
+<nav className="flex justify-center gap-4 p-4 bg-amber-800 text-amber-50 font-serif border-b-4 border-amber-900">
+  <button 
+    onClick={() => setZakladka('home')} 
+    className={`px-3 py-1 border border-amber-50 rounded transition-colors hover:bg-amber-700 ${zakladka === 'home' ? 'bg-amber-600 font-bold' : ''}`}
+  >🏠 Start</button>
+  <button 
+    onClick={() => setZakladka('test')} 
+    className={`px-3 py-1 border border-amber-50 rounded transition-colors hover:bg-amber-700 ${zakladka === 'test' ? 'bg-amber-600 font-bold' : ''}`}
+  >🧠 Test osobowości</button>
+  <button 
+    onClick={() => setZakladka('kalendarz')} 
+    className={`px-3 py-1 border border-amber-50 rounded transition-colors hover:bg-amber-700 ${zakladka === 'kalendarz' ? 'bg-amber-600 font-bold' : ''}`}
+  >📅 Kalendarz</button>
+  <button 
+    onClick={() => setZakladka('listy')} 
+    className={`px-3 py-1 border border-amber-50 rounded transition-colors hover:bg-amber-700 ${zakladka === 'listy' ? 'bg-amber-600 font-bold' : ''}`}
+  >💌 Listy</button>
+  <button 
+    onClick={() => setZakladka('zadanie')} 
+    className={`px-3 py-1 border border-amber-50 rounded transition-colors hover:bg-amber-700 ${zakladka === 'zadanie' ? 'bg-amber-600 font-bold' : ''}`}
+  >Zadanie</button>
+</nav>
 
       {zakladka === 'home' && <Glowna />}
       {zakladka === 'test' && <Test />}
--- frontend/src/Glowna.jsx
+++ frontend/src/Glowna.jsx
@@ -1,8 +1,8 @@
 function Glowna() {
   return (
     <div>
-      <h1>Alumnacja Najmniejszej Polki w Polsce 🎉</h1>
-      <img src="https://placecats.com/400/300" alt="zdjecie" width="400" />
+      <h1>Alumnacja Najmniejszej Polki w Polsce</h1>
+      <img src="/weraglowna.jpg" alt="zdjecie" width="400" class='center' />
       <p>
         Tu wpisz tekst powitania... możecie napisać cokolwiek,
         tyle ile chcecie, będzie się ładnie wyświetlać!
