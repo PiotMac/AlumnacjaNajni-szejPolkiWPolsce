729580c5601f775a41c91e47a768320e46682a01
Wiktoria Kudła <wiktoriakudla1205@gmail.com>
2026-06-07 00:55:33 +0200

    test 2

--- frontend/src/Zadanie.jsx
+++ frontend/src/Zadanie.jsx
@@ -4,7 +4,7 @@ function Zadanie() {
       <h1>Alumnowe niebo??</h1>
       <img src="https://placecats.com/400/300" alt="zdjecie" width="400" />
       <p>
-        test test test
+        test test
       </p>
     </div>
   )
